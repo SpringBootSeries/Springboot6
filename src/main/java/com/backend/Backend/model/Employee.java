59974ace7963872077f71e0bdaa0f29cdef50cd9
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 19:39:59 +0530

    Added details of the Employee model

--- src/main/java/com/backend/Backend/model/Employee.java
+++ src/main/java/com/backend/Backend/model/Employee.java
@@ -1,4 +1,26 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.List;
+
+@Entity
 public class Employee {
+    @Id
+    private String empNo;
+    private String name;
+    private int age;
+    private double salary;
+    private String gender;
+
+    @ManyToOne
+    private Department department;
+
+    @ManyToMany(mappedBy = "employees")
+    private List<Project> projects;
+
+
 }
