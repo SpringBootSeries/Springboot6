148223e68640292e8ff5049fd8c2975ea8b71b4f
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 19:41:54 +0530

    Added details of the Insurance model

--- src/main/java/com/backend/Backend/model/Insurance.java
+++ src/main/java/com/backend/Backend/model/Insurance.java
@@ -1,4 +1,19 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Insurance {
+    @Id
+    @Column(name="Insurance_id")
+    private int id;
+    @Column(name="Insurance_type")
+    private String type;
+
+    @OneToOne
+    private Employee employees;
+
 }
