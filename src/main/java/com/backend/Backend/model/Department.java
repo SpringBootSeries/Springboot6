636aba45c7c938b138e9c029c0dce2c63f050ec3
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 19:34:25 +0530

    Added details of the Department model

--- src/main/java/com/backend/Backend/model/Department.java
+++ src/main/java/com/backend/Backend/model/Department.java
@@ -1,4 +1,25 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Department {
+    @Id
+    @Column(name="dept_id")
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Date established;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
 }
