c101a284fe5426ebf944db08a4250568bf37a8c1
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 19:47:24 +0530

    Added details to the Project model

--- src/main/java/com/backend/Backend/model/Project.java
+++ src/main/java/com/backend/Backend/model/Project.java
@@ -1,4 +1,24 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@Entity
 public class Project {
+    @Id
+    @Column(name = "project_id")
+    private int id;
+    @Column(name = "project_name")
+    private String name;
+    @Column(name = "project_cost")
+    private long totalcost;
+    @ManyToMany
+    private List<Employee>employees;
+
+
 }
